4ac48b988e4fe7db2021bc17412b848625b94602
juhyejin <juhyejin.dev@gmail.com>
2023-04-10 19:41:09 +0900

    :sparkles: socket.io 연결, cors설정

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -2,14 +2,21 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const httpServer = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(httpServer, {
+  cors:{
+    origin: ['http://localhost:5173'],
+    methods:["GET","POST"],
+    credentials: true
+  }
+});
 const PORT = 3000;
 
-app.get('/', (_,res)=>{
-  res.send('Hi! This is my first express server');
+
+io.on('connection', (socket)=>{
+  console.log('연결성공!')
 })
-app.get('/*', (_,res)=>{
-  res.redirect('/')
-})
+
 httpServer.listen(PORT, () => {
   console.log('서버연결 성공!')
 })
